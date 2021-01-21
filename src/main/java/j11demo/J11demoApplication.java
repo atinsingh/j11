1164b4e6c057f748d736953135ab2178c2c11f54
Singh, Atin <atin@pragra.io>
2021-01-20 21:50:32 -0500

    Initial Code

---
+++ src/main/java/j11demo/J11demoApplication.java
@@ -0,0 +1,13 @@
+package j11demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J11demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J11demoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/j11demo/J11demoApplicationTests.java
@@ -0,0 +1,13 @@
+package j11demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J11demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
